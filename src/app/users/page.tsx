761fd83ea3678eef0d5666c2dc3ea5ddea376d97
Jay Ahn <jay@xerapix.studio>
2023-12-01 16:58:14 +0900

    Adding database connection

---
+++ src/app/users/page.tsx
@@ -0,0 +1,21 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import { useState } from "react";
+
+const prisma = new PrismaClient();
+
+export default async function Authors() {
+  const users = await prisma.users.findMany();
+
+  return (
+    <main>
+      {users.map((user) => {
+        return (
+          <div>
+            <span>id:{user.UserID}</span> <span>name:{user.Username}</span>{" "}
+            <span>email:{user.Email}</span>
+          </div>
+        );
+      })}
+    </main>
+  );
+}
