1e352d08448ddda86d4697fcfa33b3eafdbf1f57
gwlee <leemarshal@naver.com>
2023-12-13 05:27:14 +0900

    feat: AfterAuthorDeactivation Trigger

--- trigger.sql
+++ trigger.sql
@@ -11,9 +11,23 @@ CREATE TRIGGER AfterReaderDeactivation
 AFTER UPDATE ON Users
 FOR EACH ROW
 BEGIN
-    IF OLD.isActive <> NEW.IsActive AND NEW.IsActive = FALSE THEN
+    IF OLD.IsActive <> NEW.IsActive AND NEW.IsActive = FALSE THEN
         IF EXISTS (SELECT * FROM Readers WHERE ReaderID = NEW.UserID) THEN
             CALL DeactivateReader(NEW.UserID);
+        END IF;
+    END IF;
+END$$
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS AfterAuthorDeactivation;
+DELIMITER $$
+CREATE TRIGGER AfterAuthorDeactivation
+AFTER UPDATE ON Users
+FOR EACH ROW
+BEGIN
+    IF OLD.IsActive <> NEW.IsActive AND NEW.IsActive = FALSE THEN
+        IF EXISTS (SELECT * FROM Authors WHERE AuthorID = NEW.UserID) THEN
+            CALL DeactivateAuthor(NEW.UserID);
         END IF;
     END IF;
 END$$
