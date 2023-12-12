cc09d98c603806f08b52a1ea93520a9e7e3fefab
Junoh Cho <herojeff02@gmail.com>
2023-12-13 01:10:19 +0900

    Added login button to ToolBar(Home)

--- src/app/home/components/ToolBar.tsx
+++ src/app/home/components/ToolBar.tsx
@@ -1,38 +1,37 @@
 "use client";
 import React from "react";
-import ArrowLeftIcon from "../../icons/ArrowLeftIcon";
-import { useRouter } from "next/navigation";
+import {useRouter} from "next/navigation";
 import ComicIcon from "../../icons/ComicIcon";
+import Link from "next/link";
 
-export default function ToolBar({ title = "", height = 50 }) {
-  const router = useRouter();
+export default function ToolBar({title = "", height = 50}) {
+    const router = useRouter();
 
-  return (
-    <div
-      style={{
-        background: "#FFFFFFA0",
-        backdropFilter: "blur(20px)",
-        width: "100%",
-        height: height,
-        position: "fixed",
-        left: 0,
-        top: 0,
-        display: "flex",
-        justifyContent: "start",
-        alignItems: "center",
-      }}
-    >
-      <ComicIcon
-        props={{
-          style: {
-            fill: "#000",
-            width: 40,
-            height: "100%",
-            margin: "6px 14px 6px 20px",
-          },
-        }}
-      />
-      <span style={{ color: "black", fontWeight: 600 }}>{title}</span>
-    </div>
-  );
+    return (
+        <div style={{
+            background: "#FFFFFFA0",
+            backdropFilter: "blur(20px)",
+            WebkitBackdropFilter: "blur(20px)",
+            width: "100%",
+            height: height,
+            position: "fixed",
+            left: 0,
+            top: 0,
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+        }}>
+            <div style={{display: "flex", justifyContent: "start", alignItems: "center"}}>
+                <div style={{margin: "6px 14px 6px 20px",}}>
+                    <ComicIcon/>
+                </div>
+                <span style={{color: "black", fontWeight: 600}}>{title}</span>
+            </div>
+            <div style={{display: "flex", justifyContent: "start", alignItems: "center"}}>
+                <div style={{margin: 20, borderRadius: 1000}}>
+                    <Link href={"/login"}><span style={{fontWeight: 600}}>로그인</span></Link>
+                </div>
+            </div>
+        </div>
+    );
 }
