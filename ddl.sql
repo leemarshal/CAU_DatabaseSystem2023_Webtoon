c70bf1336e914959e2030ed5783f55b14f2f0de0
moonsh0678 <moonsh0678@gmail.com>
2023-12-13 00:55:49 +0900

    Revert "modified procedure and admin in ddl"
    This reverts commit b7a6172fa3661b213ad8447cf2c316e16050b944.

--- ddl.sql
+++ ddl.sql
@@ -281,8 +281,6 @@ CREATE TABLE Notice (
     Title VARCHAR(255),
     Content VARCHAR(255),
     PostedDate DATE,
-    AdminID INT,
-    FOREIGN KEY (AdminID) REFERENCES Administrator(AdministratorID),
     IsActive BOOLEAN
     -- IsActive is an ENUM to ensure it only contains 'True' or 'False'.
 );
--- procedure.sql
+++ procedure.sql
@@ -21,7 +21,7 @@ DELIMITER //
 CREATE PROCEDURE DeactivateReader(IN p_ReaderID INT)
     BEGIN
     DECLARE IsReaderActive BOOLEAN ;
-    SELECT IsActive INTO IsReaderActive FROM Users WHERE UserID = p_ReaderID;
+    SELECT IsActive INTO IsReaderActive FROM Readers WHERE ReaderID = p_ReaderID;
     IF IsReaderActive = False THEN
         UPDATE Users SET Username = 'Unknown' WHERE UserID = p_ReaderID;
         DELETE FROM ReaderBookmark WHERE ReaderID = p_ReaderID;
@@ -29,8 +29,8 @@ CREATE PROCEDURE DeactivateReader(IN p_ReaderID INT)
         DELETE FROM ReaderBookmark WHERE ReaderID = p_ReaderID;
         DELETE FROM ReaderSearch WHERE ReaderID = p_ReaderID;
         DELETE FROM UserNoticeRead WHERE ReaderID = p_ReaderID;
-        DELETE FROM Reading WHERE ReaderID = p_ReaderID;
-        UPDATE ReaderPaymentMethods SET PaymentMethodDetails = null WHERE ReaderID = p_ReaderID;
+        DELETE FROM ReaderPaymentMethods WHERE ReaderID = p_ReaderID;
+        UPDATE ReaderCookiePurchases SET ReaderPaymentMethodID = '0' WHERE ReaderID = p_ReaderID;
 
         COMMIT;
     END IF;
@@ -41,7 +41,6 @@ CREATE PROCEDURE DeactivateAuthor(IN p_AuthorID INT)
     SELECT IsActive INTO IsAuthorActive FROM Users WHERE UserID = p_AuthorID;
     IF IsAuthorActive = False THEN
        UPDATE Authors SET Salary = 0 WHERE AuthorID = p_AuthorID;
-        COMMIT;
     END IF;
 END //;
 DELIMITER ;
