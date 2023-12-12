e28a3df8f261a1861fb14647e53da20f1390f2ad
moonsh0678 <moonsh0678@gmail.com>
2023-12-12 22:01:38 +0900

    Modified procedure

--- procedure.sql
+++ procedure.sql
@@ -16,3 +16,22 @@ BEGIN
 END$$
 
 DELIMITER ;
+DELIMITER //
+CREATE PROCEDURE DeactivateReader(IN p_ReaderID INT)
+    BEGIN
+    DECLARE IsReaderActive BOOLEAN ;
+    SELECT IsActive INTO IsReaderActive FROM Readers WHERE ReaderID = p_ReaderID;
+    IF IsReaderActive = False THEN
+        UPDATE Users SET Username = 'Unknown' WHERE UserID = p_ReaderID;
+        DELETE FROM ReaderBookmark WHERE ReaderID = p_ReaderID;
+        DELETE FROM Subscriptions WHERE ReaderID = p_ReaderID;
+        DELETE FROM ReaderBookmark WHERE ReaderID = p_ReaderID;
+        DELETE FROM ReaderSearch WHERE ReaderID = p_ReaderID;
+        DELETE FROM UserNoticeRead WHERE ReaderID = p_ReaderID;
+        DELETE FROM ReaderPaymentMethods WHERE ReaderID = p_ReaderID;
+        UPDATE ReaderCookiePurchases SET ReaderPaymentMethodID = '0' WHERE ReaderID = p_ReaderID;
+
+        COMMIT;
+    END IF;
+END //;
+DELIMITER ;
